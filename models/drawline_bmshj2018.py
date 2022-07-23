5c01c1e73ceabf3592fba9c45c38e739e249c2dc
Erincrying <1016158928@qq.com>
2022-07-23 14:45:26 +0800

    更新基于柯达数据集压缩码率的图像绘制

--- models/drawline_bmshj2018.py
+++ models/drawline_bmshj2018.py
@@ -4,63 +4,64 @@ import matplotlib.pyplot as plt
 bpp_result = [0.115239, 0.185698, 0.301804, 0.468972, 0.686378, 0.966864, 1.307441, 1.727503]
 psnr_result = [27.106351, 28.679134, 30.616753, 32.554935, 34.580960, 36.720366, 38.807960, 40.794920]
 
-bpp_myself = [0.1935, 0.2553, 0.4074, 0.5314, 0.6804, 0.7794]
-psnr_myself = [27.165842, 28.450409, 31.436895, 33.45183, 35.177105, 36.161648 ]
 
+
+bpp_myself = [0.13388739691840276, 0.22194756401909724, 0.37213897705078125,0.5579639010959202, 0.7643720838758682, 0.9038340250651041]
+psnr_myself = [27.200388, 28.450409,31.436895,33.45183, 35.177105, 36.161648 ]
 
 # 第一个码率点
-bpp_myself_test = [0.1945]
+bpp_myself_test = [0.13505808512369794 ]
 psnr_myself_test = [27.207735]
 
-bpp_myself_01 = [0.1935]
+bpp_myself_01 = [0.1326226128472222]
 psnr_myself_01 = [27.165842]
 
-bpp_myself_02 = [0.2301]
+bpp_myself_02 = [0.13388739691840276 ]
 psnr_myself_02 = [27.200388]
 
-bpp_myself_03 = [0.2293]
+bpp_myself_03 = [0.13263702392578125]
 psnr_myself_03 = [25.743942]
 
 
-bpp_myself_04 = [0.2328]
+bpp_myself_04 = [0.13419257269965276]
 psnr_myself_04 = [26.97355]
 
 
 # 第二个码率点
-bpp_myself_SEC_01= [0.2553]
+bpp_myself_SEC_01= [0.22194756401909724]
 psnr_myself_SEC_01= [28.450409]
 
-bpp_myself_SEC_02= [0.2946]
+bpp_myself_SEC_02= [0.214125739203559 ]
 psnr_myself_SEC_02= [28.352106]
 
-bpp_myself_SEC_03= [0.2975]
+bpp_myself_SEC_03= [0.21606784396701392]
 psnr_myself_SEC_03= [28.341482]
 
 
 # 第三个码率点
-bpp_myself_THI_01= [0.3598]
+bpp_myself_THI_01= [0.36642201741536456]
 psnr_myself_THI_01= [30.567152]
 
-bpp_myself_THI_02= [0.4074]
+bpp_myself_THI_02= [0.37213897705078125 ]
 psnr_myself_THI_02= [31.436895]
 
 
-bpp_myself_THI_03= [0.4128]
+bpp_myself_THI_03= [0.37700144449869794]
 psnr_myself_THI_03= [31.440132]
 
 
 # 第四个码率点
-bpp_myself_FOUR_01= [0.5314]
+bpp_myself_FOUR_01= [0.5579639010959202]
 psnr_myself_FOUR_01= [33.45183]
 
 
 # 第五个码率点
-bpp_myself_FIVE_01= [0.6804]
+bpp_myself_FIVE_01= [0.7643720838758682]
 psnr_myself_FIVE_01= [35.177105]
 
 
 # 第六个码率点
-bpp_myself_SIX_01= [0.7794]
+bpp_myself_SIX_01= [0.9038340250651041]
 psnr_myself_SIX_01= [36.161648 ]
 
 # 添加横纵坐标与标题
